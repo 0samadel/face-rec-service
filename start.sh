1adc2c76c42f717432aeb8fa3b2b7fd75a0ed8fa
OSAMA ADEL <intelliface.tech@gmail.com>
2025-06-13 22:25:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 # This command will start the Gunicorn server.
 # It binds to all network interfaces on the port provided by Render's $PORT variable.
-gunicorn --bind 0.0.0.0:$PORT app:app
+gunicorn --bind 0.0.0.0:$PORT --timeout 120 app:app
